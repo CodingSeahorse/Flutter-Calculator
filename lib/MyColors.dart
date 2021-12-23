badfbdc6ed55ba1e5703c99f0176dc074783eee6
Sarah Lena De Matos Silva Knell <69032664+BeSunrise@users.noreply.github.com>
2021-12-23 21:23:10 +0100

    added my colors

---
+++ lib/MyColors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyColors{
+  static final myBaseBackgroundColor = Color.fromRGBO(52, 67, 69,1);
+  static final myTitleBackgroundColor = Color.fromRGBO(19, 188, 209,1);
+  static final myOpsBackgroundColor = Color.fromRGBO(111, 198, 209,1);
+  static final myOpsBorderColor = Color.fromRGBO(89, 162, 171, 1);
+  static final mySpezOpsBackgroundColor = Color.fromRGBO(130, 132, 133,1);
+  static final mySpezOpsBorderColor = Color.fromRGBO(97, 99, 99,1);
+  static final myNumberBtnColor = Color.fromRGBO(35, 51, 51, 1);
+  static final myNumberBtnBorderColor = Color.fromRGBO(20, 36, 36, 1);
+  static final myBtnTextColor = Color.fromRGBO(213, 235, 234,1);
+}
