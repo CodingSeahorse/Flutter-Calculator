d718ff8d14e3befbab7e80bccb92112a8e04f05d
Sarah Lena De Matos Silva Knell <69032664+BeSunrise@users.noreply.github.com>
2021-12-24 18:35:11 +0100

    much better result

--- lib/CalculatorLogic.dart
+++ lib/CalculatorLogic.dart
@@ -13,8 +13,8 @@ class CalculatorLogic{
 
   dynamic result = '';
   dynamic finalResult = '';
-  dynamic opr = '';
-  dynamic preOpr = '';
+  dynamic operation = '';
+  dynamic preOperation = '';
 
   void calculate(btnText) {
     if (btnText == 'AC') {
@@ -23,41 +23,55 @@ class CalculatorLogic{
       secondNumber = 0;
       result = '';
       finalResult = '0';
-      opr = '';
-      preOpr = '';
-    } else if (opr == '=' && btnText == '=') {
-      if (preOpr == '+') {
-        finalResult = SimpleMath.add(firstNumber, secondNumber);
-      } else if (preOpr == '-') {
-        finalResult = SimpleMath.sub(firstNumber, secondNumber);
-      } else if (preOpr == 'x') {
-        finalResult = SimpleMath.mul(firstNumber, secondNumber);
-      } else if (preOpr == '/') {
-        finalResult = SimpleMath.div(firstNumber, secondNumber);
-      }
-    } else
-    if (btnText == '+' || btnText == '-' || btnText == 'x' || btnText == '/' ||
-        btnText == '=') {
+      operation = '';
+      preOperation = '';
+    } else if (operation == '=' && btnText == '=') {
+        switch(preOperation){
+          case '+':
+            finalResult = SimpleMath.add(firstNumber, secondNumber);
+            break;
+          case '-':
+            finalResult = SimpleMath.sub(firstNumber, secondNumber);
+            break;
+          case 'x':
+            finalResult = SimpleMath.mul(firstNumber, secondNumber);
+            break;
+          case '/':
+            finalResult = SimpleMath.div(firstNumber, secondNumber);
+            break;
+        }
+    } else if (
+      btnText == '+' ||
+      btnText == '-' ||
+      btnText == 'x' ||
+      btnText == '/' ||
+      btnText == '=') {
+
       if (firstNumber == 0) {
         firstNumber = double.parse(result);
       } else {
         secondNumber = double.parse(result);
       }
 
-      if (opr == '+') {
-        finalResult = SimpleMath.add(firstNumber, secondNumber);
-      } else if (opr == '-') {
-        finalResult = SimpleMath.sub(firstNumber, secondNumber);
-      } else if (opr == 'x') {
-        finalResult = SimpleMath.mul(firstNumber, secondNumber);
-      } else if (opr == '/') {
-        finalResult = SimpleMath.div(firstNumber, secondNumber);
+      switch(operation){
+        case '+':
+          finalResult = SimpleMath.add(firstNumber, secondNumber);
+          break;
+        case '-':
+          finalResult = SimpleMath.sub(firstNumber, secondNumber);
+          break;
+        case 'x':
+          finalResult = SimpleMath.mul(firstNumber, secondNumber);
+          break;
+        case '/':
+          finalResult = SimpleMath.div(firstNumber, secondNumber);
+          break;
       }
-      preOpr = opr;
-      opr = btnText;
+
+      preOperation = operation;
+      operation = btnText;
       result = '';
-    }
-    else if (btnText == '%') {
+    } else if (btnText == '%') {
       result = firstNumber / 100;
       finalResult = SimpleMath.checkDecimalResult(result);
     } else if (btnText == '.') {
@@ -65,109 +79,7 @@ class CalculatorLogic{
         result = result.toString() + '.';
       }
       finalResult = result;
-    }
-
-    else if (btnText == '+/-') {
-      result.toString().startsWith('-') ?
-      result = result.toString().substring(1) : result =
-          '-' + result.toString();
-      finalResult = result;
-    }
-
-    else {
-      result = result + btnText;
-      finalResult = result;
-    }
-    /*dynamic text = '0';
-  dynamic firstNumber = 0;
-  dynamic secondNumber = 0;
-
-  dynamic result = '';
-  dynamic finalResult = '';
-  dynamic operator = '';
-  dynamic preOperator = '';
-
-  dynamic calculate(btnText) {
-
-    if(btnText == 'AC'){ // <<-----  Case AC (Reset) ----->>
-      text = '0';
-      firstNumber = 0;
-      secondNumber = 0;
-
-      result = '';
-      finalResult = '';
-      operator = '';
-    } else if( // <<-----  Case Pressed '=' ----->>
-      operator == '=' &&
-      btnText == '=') {
-
-      switch(preOperator){
-        case '+':
-          finalResult = SimpleMath.add(firstNumber,secondNumber);
-          break;
-        case '-':
-          finalResult = SimpleMath.sub(firstNumber,secondNumber);
-          break;
-        case 'x':
-          finalResult = SimpleMath.mul(firstNumber, secondNumber);
-          break;
-        case '/':
-          finalResult = SimpleMath.div(firstNumber, secondNumber);
-          break;
-      }
-
-      return finalResult;
-    } else if( // <<-----  Case Pressed Operator ----->>
-        btnText == '+' ||
-        btnText == '-' ||
-        btnText == 'x' ||
-        btnText == '/' ||
-        btnText == '='){
-
-      if(firstNumber == 0) {
-        firstNumber = double.parse(Converter.eraseBigResult(result));
-        developer.log('firstNumber: ' + firstNumber);
-      } else {
-        secondNumber = double.parse(Converter.eraseBigResult(result));
-        developer.log('secondNumber: ' + firstNumber);
-      }
-
-      switch(operator){
-        case '+':
-          finalResult = SimpleMath.add(firstNumber,secondNumber);
-          break;
-        case '-':
-          finalResult = SimpleMath.sub(firstNumber,secondNumber);
-          break;
-        case 'x':
-          finalResult = SimpleMath.mul(firstNumber, secondNumber);
-          break;
-        case '/':
-          finalResult = SimpleMath.div(firstNumber, secondNumber);
-          break;
-      }
-      preOperator = operator;
-      operator = btnText;
-      result = '';
-    } else if (btnText == '%'){ // <<-----  Case % ----->>
-      //
-      // We divide the firstNumber to get the percent
-      //
-      result = firstNumber / 100;
-      finalResult = SimpleMath.checkDecimalResult(result);
-
-      return finalResult;
-    } else if (btnText == ',') { // <<-----  Case Decimal ----->>
-      //
-      // We check if the result is already a decimal Number
-      //
-      if(!result.toString().contains(',')) {
-        result = result.toString() + ',';
-      }
-
-      finalResult = result;
-      return finalResult;
-    } else if (btnText == '+/-'){ // <<-----  Case +/- ----->>
+    } else if (btnText == '+/-') {
       //
       // We check if the result is negative or not
       //
@@ -178,44 +90,14 @@ class CalculatorLogic{
       }
 
       finalResult = result;
-      return finalResult;
-
-    } else if(result.toString().length <= 10){ // <<-----  Case #Number ----->>
-
-      //result = result + btnText;
-
-      if(
-        result.toString().length > 3 &&
-        !result.toString().contains('.')){
-        //
-        // We check if the result length is > 3 and doesn't contain a '.'
-        //
-        Converter.createBigResult(result);
-
-        result = result + btnText;
-        finalResult = result;
-        return finalResult;
-      }else if(
-          result.toString().length > 3 &&
-          result.toString().contains('.')) {
-        //
-        // We check if the result length is > 3 and already contain a '.'
-        //
-        List<String> splicedResult = result.toString().split('.');
-
-        if(splicedResult[1].length > 3){
-          //
-          // We check if the 2nd part String is > 3
-          //
-          dynamic croppedResult = StringUtils.addCharAtPosition(splicedResult[1], ".", 3, repeat: true);
-          result = splicedResult[0] + '.' + croppedResult.toString();
-        }
-
-        result = result + btnText;
-        finalResult = result;
-        return finalResult;
-      }
+    } /*else if (result.toString().length > 3){ TODO: FIX LOGIC
+      result = Converter.checkBigResult(result);
+    } else if(result.toString().length >= 10){
+      developer.log("Yes result longer than 10 ");
+        result = Converter.eraseBigResult(result);*/
+     else {
+      result = result + btnText;
+      finalResult = result;
     }
-  }*/
   }
 }
--- lib/utils/Converter.dart
+++ lib/utils/Converter.dart
@@ -16,7 +16,22 @@ class Converter {
     return result;
   }
 
-  static String createBigResult(result) {
-    return StringUtils.addCharAtPosition(result, ".", 3, repeat: true);
+  static String checkBigResult(result) {
+    if(!result.toString().contains('.')){
+      return StringUtils.addCharAtPosition(result, ".", 3, repeat: true);
+    }else{
+      List<String> splicedResult = result.toString().split('.');
+
+      if (splicedResult[1].length > 3) {
+        //
+        // We check if the 2nd part String is > 3
+        //
+        dynamic croppedResult = StringUtils.addCharAtPosition(
+            splicedResult[1], ".", 3, repeat: true);
+        result = splicedResult[0] + '.' + croppedResult.toString();
+      }
+      return result;
+    }
+
   }
 }
